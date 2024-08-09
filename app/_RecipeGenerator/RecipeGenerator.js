7478aac3f05c054c4139149725446914635537ba
Thanh Duy Trinh <thanhtrinh501@Thanhs-MacBook-Pro-2.local>
2024-08-08 20:11:53 -0700

    Generate random recipe

--- app/_RecipeGenerator/RecipeGenerator.js
+++ app/_RecipeGenerator/RecipeGenerator.js
@@ -1,6 +1,9 @@
+'use client'
 import { Box, Button, Divider } from '@mui/material';
-import React from 'react'
+import React, { useState } from 'react'
 import GettingRecipe from './GettingRecipe';
+import InitialRun from './InitialRun';
+import NoSelectedItems from './NoSelectedItems';
 
 const StyledChartBox = {
     backgroundColor: 'white',
@@ -9,20 +12,29 @@ const StyledChartBox = {
     gridColumn: '3 / span 2',
     width: '30%',
     height: '56.4vh',
+    display: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   };
 export default function RecipeGenerator({displayRecipe, handleDisplayRecipe, prevSelectedItems }) {
   const query = prevSelectedItems.map(item => item.toLowerCase()).join('+');
+  const [trigger, setTrigger] = useState(false);
+  function handleGenerate(){
+    handleDisplayRecipe();
+    setTrigger(trigger => !trigger);
+  }
   return (
     <Box sx={StyledChartBox}>
-        <Box sx={{display: 'flex', justifyContent: 'center'}}>
-          <Button sx={{mt: '5px', border: '1px solid black'}} onClick={handleDisplayRecipe}>
+        <Box sx={{display: 'flex',justifyContent: 'center'}}>
+          <Button sx={{mt: '5px', border: '1px solid black'}} onClick={handleGenerate}>
             Generate
           </Button>
         </Box>
-        <Divider sx={{bgcolor: 'black', mt: '5px', pt: '1px'}}/>
-        {prevSelectedItems.length === 0 && !displayRecipe && <p>Select items to generate recipe</p>}
-        {displayRecipe && prevSelectedItems.length === 0 && <p>You haven't selected any items</p>}
-        {displayRecipe && prevSelectedItems.length > 0 && <GettingRecipe query={query}/>}
+        <Divider sx={{bgcolor: 'black', mt: '5px', pt: '1px', mb: '15px'}}/>
+        {prevSelectedItems.length === 0 && !displayRecipe && <InitialRun/>}
+        {displayRecipe && prevSelectedItems.length === 0 && <NoSelectedItems/>}
+        {displayRecipe && prevSelectedItems.length > 0 && <GettingRecipe query={query} trigger={trigger}/>}
+        
     </Box>
   )
 }
